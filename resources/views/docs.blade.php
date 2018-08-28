78e9e230a0381bc16a7e825f36bbae5647b4b69d
Saleem Hadad <saleem_ha@hotmail.com>
2018-08-28 14:48:42 +0800

    update views

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,1 +1,37 @@
-{{ $content }}
+<style>
+    .box {
+        background-color: #eaeaea;
+        padding: 20px;
+        margin-bottom: 20px;
+    }
+</style>
+
+<div class="box">
+    {{ $title }}
+</div>
+
+<div class="box">
+    {!! $index !!}
+</div>
+
+<div class="box">
+    {!! $content !!}
+</div>
+
+<div class="box">
+    {{ $currentVersion }}
+</div>
+
+<div class="box">
+    @foreach($versions as $version)
+        {{ $version }}
+    @endforeach
+</div>
+
+<div class="box">
+    {{ $currentSection }}
+</div>
+
+<div class="box">
+    {{ $canonical }}
+</div>
