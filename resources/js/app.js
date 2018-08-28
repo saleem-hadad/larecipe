4a5ac5cf157c8d2941d0e4fb0166fd2741eddabc
Saleem Hadad <saleem_ha@hotmail.com>
2018-08-28 19:00:39 +0800

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ window.jQuery = window.$ = require('./vendor/jquery.js');
 require('./vendor/prism.js');
 require('./vendor/bootstrap.js');
 require('./vendor/typeahead.js');
-require('./vendor/scotchPanels.js');
 
 // Standalone vendor libraries
 const Mousetrap = require('./vendor/mousetrap.js');
@@ -26,52 +25,7 @@ new Vue({
 
 jQuery(function($) {
 
-  // Smooth scroll to anchor
-  $('body.home a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-
-  var scotchPanel = $('#slide-menu').scotchPanel({
-    containerSelector: 'body',
-    direction: 'left',
-    duration: 300,
-    transition: 'ease',
-    distanceX: '70%',
-    forceMinHeight: true,
-    minHeight: '2500px',
-    enableEscapeKey: true
-  }).show(); // show to avoid flash of content
-
-  $('.toggle-slide').click(function() {
-    scotchPanel.css('overflow', 'scroll');
-    scotchPanel.toggle();
-    return false;
-  });
-
-  $('.overlay').click(function() {
-    // CLOSE ONLY
-    scotchPanel.close();
-  });
-
-  // Hide the slide menu when changing the browser width
-
-  function checkSize() {
-    if (window.matchMedia("(min-width: 960px)").matches) {
-      scotchPanel.close();
-    }
-  }
-
-  checkSize();
-  window.onresize = checkSize;
+  
 
   // gheading links
   $('.docs-wrapper').find('a[name]').each(function () {
