e3b7125eff2400c02e223c63a683392a8e64f34e
Saleem Hadad <saleem_ha@hotmail.com>
2018-08-29 16:23:11 +0800

    update config

--- publishable/config/larecipe.php
+++ publishable/config/larecipe.php
@@ -1,17 +1,55 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Routes
+    |--------------------------------------------------------------------------
+    |
+    | These options configure the behavior of the Larecipe docs basic routes
+    | where you can speicify the url of your documentations, the location
+    | of your docs and the landing page when a user visits /docs route.
+    |
+    |
+    */
+
     'docs'      => [
         'route'   => '/docs',
         'path'    => '/resources/docs',
         'landing' => 'overview'
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Versions
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify and set the versions and the default (latest) one
+    | of your documentation's versions where you can redirect the user to
+    | Just make sure that the default version is in the published list.
+    |
+    |
+    */
+
     'versions'      => [
         'default'   => '1.0',
         'published' => [
             '1.0'
         ]
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Settings
+    |--------------------------------------------------------------------------
+    |
+    | These options configure the additional behaviors of your documentation
+    | where you can limit the access to only authinticated users in your
+    | system, change the default theme and colors of the docs layout.
+    |
+    |
+    */
+
     'settings' => [
         'auth' => false
     ]
