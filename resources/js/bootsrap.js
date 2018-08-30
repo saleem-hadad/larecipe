f6e3737fdafec4b0008a717cd2a6b917ad3d02e9
Saleem Hadad <saleem_ha@hotmail.com>
2018-08-30 17:22:39 +0800

    add smooth scrolling library, update ui

--- resources/js/bootsrap.js
+++ resources/js/bootsrap.js
@@ -1,6 +1,7 @@
+import "../sass/vendor/nucleo/css/nucleo.css";
+import "../sass/vendor/font-awesome/css/font-awesome.css";
+
 window.jQuery = window.$ = require('jquery');
 
 require('./vendor/prism.js');
-
-import "../sass/vendor/nucleo/css/nucleo.css";
-import "../sass/vendor/font-awesome/css/font-awesome.css";
+require('smoothscroll-for-websites');
--- resources/js/components/LarecipeNav.vue
+++ resources/js/components/LarecipeNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar"
+    <nav class="navbar has-bottom-border"
          :class="[
             {'navbar-expand-lg': expand},
             {[`navbar-${effect}`]: effect},
