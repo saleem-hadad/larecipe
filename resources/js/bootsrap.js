d03ddba99e07dff4f44141847454499313fb1692
Saleem Hadad <saleem_ha@hotmail.com>
2018-09-01 01:44:03 +0800

    add vue directive

--- resources/js/bootsrap.js
+++ resources/js/bootsrap.js
@@ -1,6 +1,3 @@
-import "../sass/vendor/nucleo/css/nucleo.css";
-import "../sass/vendor/font-awesome/css/font-awesome.css";
-
 window.jQuery = window.$ = require('jquery');
 
 require('./vendor/prism.js');
