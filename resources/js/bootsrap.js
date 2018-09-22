39407e4e8f02698f6acd80be87a04dedb14e6dbd
Matthew Rhodes <tankerkiller125@gmail.com>
2018-09-22 02:10:18 -0400

    Change Prism to Autoload languages

--- resources/js/bootsrap.js
+++ resources/js/bootsrap.js
@@ -1,4 +1,8 @@
 window.jQuery = window.$ = require('jquery');
 
 require('./vendor/prism.js');
+// This enables all language support via CDN
+Prism.plugins.autoloader.use_minified = true;
+Prism.plugins.autoloader.languages_path = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/components/';
+
 require('smoothscroll-for-websites');
