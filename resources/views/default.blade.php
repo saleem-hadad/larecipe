193b0d123f1ececaed32151aec01a3c6a55ed478
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-05 06:54:41 +0800

    Better title tag

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,7 +3,7 @@
     <head>
         {{-- Meta --}}
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>{{ isset($title) ? $title . ' - ' : null }}{{ config('app.name') }}</title>
+        <title>{{ isset($title) ? $title . ' | ' : null }}{{ config('app.name') }}</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
