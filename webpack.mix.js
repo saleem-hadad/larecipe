c1b4a24348d3690a0ce6afecb17bd304b3ac0b49
Saleem Hadad <saleem_ha@hotmail.com>
2019-02-11 03:11:04 +0800

    improve internal search UI

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const { mix } = require('laravel-mix');
 mix.options({processCssUrls: false})
 	.sass('resources/sass/app.scss', 'publishable/assets/css')
 	.js('resources/js/app.js', 'publishable/assets/js')
-	.copy('publishable/assets', '../web/public/vendor/binarytorch/larecipe/assets');
+	.copy('publishable/assets', '../dev/public/vendor/binarytorch/larecipe/assets');
