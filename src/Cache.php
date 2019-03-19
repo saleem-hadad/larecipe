bf3b9e232dd7835df85086cc0c7d5df25a61176f
Raphael Cunha <rbcunhadesign@gmail.com>
2019-03-19 13:18:41 -0400

    Added test to make sure TTL gets changed

--- src/Cache.php
+++ src/Cache.php
@@ -37,14 +37,26 @@ class Cache
             return $callback();
         }
 
-        $cachePeriod = config('larecipe.cache.period');
-
-        $app_version = explode('.', app()->version());
-        
-        if (((int) $app_version[0] == 5 && (int) $app_version[1] >= 8) || $app_version[0] > 5) {
-            $cachePeriod = config('larecipe.cache.period') * 60;
-        }
+        $cachePeriod = $this->checkTtlNeedsChanged(config('larecipe.cache.period'));
 
         return $this->cache->remember($key, $cachePeriod, $callback);
+    }
+
+    /**
+     * Checks if minutes need to be changed to seconds
+     *
+     * @param $ttl
+     *
+     * @return float|int
+     */
+    public function checkTtlNeedsChanged($ttl)
+    {
+        $app_version = explode('.', app()->version());
+
+        if (((int) $app_version[0] == 5 && (int) $app_version[1] >= 8) || $app_version[0] > 5) {
+            return config('larecipe.cache.period') * 60;
+        }
+
+        return $ttl;
     }
 }
