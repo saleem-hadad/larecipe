4d97dd79681038651af1d49cfed4399699c77cf8
Saleem Hadad <saleem_ha@hotmail.com>
2019-07-26 13:54:41 +0400

    Update BuiltInSearchTest.php

--- tests/Feature/BuiltInSearchTest.php
+++ tests/Feature/BuiltInSearchTest.php
@@ -26,7 +26,6 @@ class BuiltInSearchTest extends TestCase
         Config::set('larecipe.search.enabled', true);
         Config::set('larecipe.search.default', 'internal');
 
-        $this->withoutExceptionHandling();
         $this->get('/docs/search-index/1.0')
             ->assertStatus(200)
             ->assertJsonStructure([
@@ -36,7 +35,5 @@ class BuiltInSearchTest extends TestCase
                     'headings'
                 ]
             ]);
-
-        $this->assertTrue(true);
     }
 }
