e9b49da5518ff07ea2e4f2c1985e1eb5482cef32
Saleem Hadad <saleem.hadad@talabat.com>
2021-05-11 05:19:58 +0400

    update Document model

--- src/Models/Document.php
+++ src/Models/Document.php
@@ -2,8 +2,52 @@
 
 namespace BinaryTorch\LaRecipe\Models;
 
+use BinaryTorch\LaRecipe\Exceptions\FillUnknownPropertyException;
+
 class Document
 {
-    // name
-    // seo
+    protected $seo;
+    protected $path;
+    protected $title;
+    protected $content;
+    protected $canonical;
+
+    public static function create($attributes = [])
+    {
+        $document = new static();
+
+        $document->fill($attributes);
+
+        return $document;
+    }
+
+    public function fill($attributes = [])
+    {
+        foreach($attributes as $key => $value) {
+            if(property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }else {
+                throw new FillUnknownPropertyException(sprintf(
+                    'Fill [%s] to unknown property on [%s].',
+                    $key, get_class($this)
+                ));
+            }
+        }
+    }
+
+    public function toArray() 
+    {
+        return [
+            'seo' => $this->seo,
+            'path' => $this->path,
+            'title' => $this->title,
+            'content' => $this->content,
+            'canonical' => $this->canonical
+        ];
+    }
+
+    public function __get($key)
+    {
+        return property_exists($this, $key) ? $this->{$key} : null;
+    }
 }
--- tests/Unit/DocumentTest.php
+++ tests/Unit/DocumentTest.php
@@ -5,21 +5,61 @@ namespace BinaryTorch\LaRecipe\Tests\Unit;
 use Illuminate\Support\Facades\Config;
 use BinaryTorch\LaRecipe\Tests\TestCase;
 use BinaryTorch\LaRecipe\Models\Document;
+use BinaryTorch\LaRecipe\Exceptions\FillUnknownPropertyException;
 
 class DocumentTest extends TestCase
 {
-    protected $document;
-
-    public function setUp(): void
+    /** @test */
+    public function it_can_be_created_from_static_create_method_without_attributes()
     {
-        parent::setUp();
+        $document = Document::create();
 
-        $this->document = $this->app->make(Document::class);
+        $this->assertInstanceOf(Document::class, $document);
     }
 
     /** @test */
-    public function it_test()
+    public function it_can_be_returned_as_array()
     {
-        $this->assertTrue(true);
+        $document = Document::create();
+
+        $this->assertArrayHasKey('title', $document->toArray());
+        $this->assertArrayHasKey('content', $document->toArray());
+    }
+
+    /** @test */
+    public function it_fill_properties_from_static_create_method()
+    {
+        $document = Document::create(['title' => 'Home page']);
+
+        $this->assertEquals('Home page', $document->title);
+    }
+
+    /** @test */
+    public function it_fill_properties_from_fill_method()
+    {
+        $document = Document::create();
+
+        $document->fill(['title' => 'Home page']);
+
+        $this->assertEquals('Home page', $document->title);
+    }
+
+    /** @test */
+    public function it_may_not_fill_unknown_property()
+    {
+        $document = Document::create();
+
+        $this->expectException(FillUnknownPropertyException::class);
+
+        $document->fill(['foo' => 'Home page']);
+    }
+
+    /** @test */
+    public function can_get_property_using_magic_method_get()
+    {
+        $document = Document::create(['content' => 'Body']);
+
+        $this->assertEquals(null, $document->title);
+        $this->assertEquals('Body', $document->content);
     }
 }
