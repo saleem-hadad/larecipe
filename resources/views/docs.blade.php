f3bb1d75e37065e95e0f73750da18ee97fc12c65
Ammar Al-Khawaldeh <me@ammar.dev>
2022-01-10 19:36:02 +0200

    Display authors information in the doc page

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -5,6 +5,15 @@
 	@include('larecipe::partials.sidebar')
 	
 	<div class="documentation is-{{ config('larecipe.ui.code_theme') }}" :class="{'expanded': ! sidebar}">
+		@if($authors)
+			<div class="text-xs text-grey-darker mb-6">
+				@if(($authorsCount = count($authors)) > 1)
+					{{$authorsCount}} authors ($authors[0]['name'] and others)
+				@else
+					By {{$authors[0]['name']}}
+				@endif
+			</div>
+		@endif
 		{!! $content !!}
 		@include('larecipe::plugins.forum')
 	</div>
