5230fce786dc04e25b819c233a1a953891241fb9
Ammar Al-Khawaldeh <me@ammar.dev>
2022-08-07 19:19:38 +0300

    implement testing git config check

--- tests/Feature/DisplayGitAuthorsTest.php
+++ tests/Feature/DisplayGitAuthorsTest.php
@@ -3,13 +3,22 @@
 namespace BinaryTorch\LaRecipe\Tests\Feature;
 
 use BinaryTorch\LaRecipe\Tests\TestCase;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use BinaryTorch\LaRecipe\Contracts\GitService as GitServiceContract;
+use BinaryTorch\LaRecipe\Tests\Fixtures\DummyGitService;
 
 class DisplayGitAuthorsTest extends TestCase
 {
     /** @test */
     public function if_git_config_is_not_enabled_page_will_be_displayed_without_errors()
     {
-        # code...
+        Config::set('larecipe.git.enabled', false);
+        
+        App::instance(GitServiceContract::class, new DummyGitService(true, []));
+
+        $this->get('/docs/1.0')
+            ->assertViewHas('authors', null);
     }
 
     /** @test */
