f1ce23bc4fcff2a601abddb75f58ffd02822881c
Ammar Al-Khawaldeh <me@ammar.dev>
2022-08-07 18:28:16 +0300

    Initialize authors feature test class

---
+++ tests/Feature/DisplayGitAuthorsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BinaryTorch\LaRecipe\Tests\Feature;
+
+use BinaryTorch\LaRecipe\Tests\TestCase;
+
+class DisplayGitAuthorsTest extends TestCase
+{
+    /** @test */
+    public function if_git_config_is_not_enabled_page_will_be_displayed_without_errors()
+    {
+        # code...
+    }
+
+    /** @test */
+    public function if_git_is_not_installed_page_will_be_displayed_without_errors()
+    {
+        # code...
+    }
+
+    /** @test */
+    public function if_git_is_installed_but_no_authors_are_found_page_will_be_displayed_without_errors()
+    {
+        # code...
+    }
+}
